edf5991d1e785939bd078371adbcb70a1343ad12
cho123nm <145460861+cho123nm@users.noreply.github.com>
2024-10-20 19:32:02 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('flowerBtn').addEventListener('click', function() {
+    const flower = document.getElementById('flower');
+    flower.classList.remove('hidden');
+    flower.style.opacity = 1;
+});
